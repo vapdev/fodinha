0273b16f7788976ad260bcf015b6096a45a70105
vitor <vpbohn@gmail.com>
2023-07-01 04:18:45 -0300

    animaçoes e bug fixes

--- src/components/Mesa.vue
+++ src/components/Mesa.vue
@@ -6,7 +6,6 @@ const cartasPlayer2 = ref([])
 const cartasPlayer4 = ref([])
 const cartasPlayer3 = ref([])
 const vira = ref(null)
-const draggable = ref(false)
 const jogadaPlayer1 = ref(null)
 const jogadaPlayer2 = ref(null)
 const jogadaPlayer3 = ref(null)
@@ -14,8 +13,6 @@ const jogadaPlayer4 = ref(null)
 const turno = ref(1)
 const rodada = ref(1)
 const manilha = ref(null)
-const pontosTimeA = ref(0)
-const pontosTimeB = ref(0)
 const passandoRodada = ref(false)
 const timeVencedorTurno1 = ref(null)
 const timeVencedorTurno2 = ref(null)
@@ -54,6 +51,7 @@ const passaTurno = () => {
 }
 
 const contaPontos = () => {
+  console.log("'Conta pontos' foi chamado")
   let countTimeA = 0
   let countTimeB = 0
   if (timeVencedorTurno1.value == 'A') {
@@ -71,6 +69,8 @@ const contaPontos = () => {
   } else {
     countTimeB++
   }
+  console.log('countTimeA', countTimeA)
+  console.log('countTimeB', countTimeB)
   if (countTimeA > countTimeB) {
     match.incrementarPontosTimeA();
     timeVencedorRodada.value = 'A'
@@ -79,11 +79,14 @@ const contaPontos = () => {
     timeVencedorRodada.value = 'B'
   }
   passandoPonto.value = true
+  timeVencedorTurno1.value = null
+  timeVencedorTurno2.value = null
+  timeVencedorTurno3.value = null
+  embaralha()
+  darAsCartas()
   setTimeout(() => {
     passandoPonto.value = false
   }, 5000)
-  embaralha()
-  darAsCartas()
 }
 
 
@@ -101,6 +104,12 @@ watch(prontoPraContar, (value) => {
   }
 })
 
+watch(rodada, (value) => {
+  if (value === 4) {
+    rodada.value = 1
+  }
+})
+
 const passaRodada = () => {
   jogadaPlayer1.value = null
   jogadaPlayer2.value = null
@@ -111,7 +120,6 @@ const passaRodada = () => {
     passandoRodada.value = false
   }, 5000)
   rodada.value++
-  vira.value = baralho.value.pop()
 }
 
 const turnoCompleto = computed(() => {
@@ -125,7 +133,9 @@ watch(turnoCompleto, (value) => {
   if (value) {
     calculaVencedor()
     atribuiTimeVencedorTurno()
-    passaRodada()
+    setTimeout(() => {
+      passaRodada()
+    }, 2000)
   }
 })
 
@@ -135,6 +145,20 @@ const retornaVencedorDaManilha = (jogadas) => {
     return a.naipe.valor - b.naipe.valor
   })[jogadas_com_manilha.length - 1]
   return jogada
+}
+
+const mapeiaValor = (valor) => {
+  if (valor == manilha.value) return 100
+  if (valor == '4') return 4
+  if (valor == '5') return 5
+  if (valor == '6') return 6
+  if (valor == '7') return 7
+  if (valor == 'Q') return 11
+  if (valor == 'J') return 12
+  if (valor == 'K') return 13
+  if (valor == 'A') return 14
+  if (valor == '2') return 15
+  if (valor == '3') return 16
 }
 
 const calculaVencedor = () => {
@@ -149,6 +173,7 @@ const calculaVencedor = () => {
     const valorB = mapeiaValor(b.valor)
     return valorA - valorB
   })
+
   let vencedor = jogadasOrdenadas[3]
 
   if (jogadasOrdenadas.some((jogada) => jogada.valor === manilha.value)) {
@@ -236,20 +261,8 @@ const jogadorVencedorComputed = computed(() => {
   if (jogador === 4) return 'Player 4'
 })
 
-const mapeiaValor = (valor) => {
-  if (valor === manilha.value) return 11
-  if (valor === '4') return 1
-  if (valor === '5') return 2
-  if (valor === '6') return 3
-  if (valor === '7') return 4
-  if (valor === 'Q') return 5
-  if (valor === 'J') return 6
-  if (valor === 'K') return 7
-  if (valor === 'A') return 8
-  if (valor === '2') return 9
-  if (valor === '3') return 10
-}
 const baralho = ref([])
+
 const embaralha = () => {
   for (let naipe of naipes) {
     for (let valor of valores) {
@@ -273,12 +286,9 @@ const darAsCartas = () => {
   vira.value = baralho.value.pop()
   const index = valores.indexOf(vira.value.valor)
   manilha.value = valores[index + 1]
-  if (valores[index] == '3') manilha.value = valores[0]
-}
-
-const pescarCarta = () => {
-  const carta = baralho.value.pop()
-  cartasPlayer1.value.push(carta)
+  if (valores[index] == '3') {
+    manilha.value = valores[0]
+  }
 }
 
 onMounted(() => {
@@ -308,11 +318,19 @@ onMounted(() => {
       </div>
       <div class="h-full w-full rounded-xl flex items-center justify-center">
         <div class="w-full flex justify-center items-center">
-          <Carta v-show="jogadaPlayer2" horizontal :carta="jogadaPlayer2" />
+          <transition enter-active-class="duration-300 ease-out" enter-from-class="transform translate-x-0 left-[3vw]"
+            enter-to-class="opacity-100 translate-x-[2.5vw] left-[17vw] rotate-[-90deg]"
+            leave-active-class="duration-200 ease-in" leave-from-class="opacity-100" leave-to-class="transform">
+            <Carta v-show="jogadaPlayer2" class="absolute" horizontal :carta="jogadaPlayer2" />
+          </transition>
         </div>
-        <div class="relative h-full w-1/3 flex flex-col items-center justify-center">
-          <div class="h-full flex items-center">
-            <Carta v-show="jogadaPlayer3" :carta="jogadaPlayer3" />
+        <div class="relative h-full flex flex-col items-center justify-center">
+          <div class="h-full flex items-center justify-center">
+            <transition enter-active-class="duration-300 ease-out" enter-from-class="transform translate-y-0 -top-[8.2vw]"
+              enter-to-class="opacity-100 translate-y-[10.3vw] -top-[8.2vw] rotate-[-180deg]"
+              leave-active-class="duration-200 ease-in" leave-from-class="opacity-100" leave-to-class="transform">
+              <Carta v-show="jogadaPlayer3" class="absolute" :carta="jogadaPlayer3" />
+            </transition>
           </div>
           <div class="flex items-center justify-center">
             <!-- <div class="w-[5vw] text-[1vw] flex items-center justify-center"> -->
@@ -324,25 +342,30 @@ onMounted(() => {
           <div class="h-full flex flex-col w-[5vw] justify-center group items-center">
             <transition enter-active-class="duration-300 ease-out"
               enter-from-class="transform -translate-y-0 -bottom-[8.2vw]"
-              enter-to-class="opacity-100 -translate-y-[10.3vw] -bottom-[8.125vw] rotate-180"
+              enter-to-class="opacity-100 -translate-y-[10.3vw] -bottom-[8.2vw] rotate-[-180deg]"
               leave-active-class="duration-200 ease-in" leave-from-class="opacity-100" leave-to-class="transform">
               <Carta v-show="jogadaPlayer1" class="absolute" :carta="jogadaPlayer1" />
             </transition>
           </div>
         </div>
         <div class="w-full flex justify-center items-center">
-          <Carta v-show="jogadaPlayer4" horizontal :carta="jogadaPlayer4" />
+          <transition enter-active-class="duration-300 ease-out"
+            enter-from-class="transform -translate-x-0 right-[30.2vw]"
+            enter-to-class="opacity-100 -translate-x-[10vw] right-[30.2vw] rotate-[-90deg]"
+            leave-active-class="duration-200 ease-in" leave-from-class="opacity-100" leave-to-class="transform">
+            <Carta v-show="jogadaPlayer4" class="absolute" horizontal :carta="jogadaPlayer4" />
+          </transition>
         </div>
       </div>
       <div class="h-1/3 flex justify-center items-center bg-green-800">
         <div class="flex items-center flex-col">
-          <div v-show="jogadaVencedora || passandoRodada" class="absolute bottom-[28.5vw] text-[2vw] text-white">
-            <span v-if="jogadaVencedora">
+          <div class="absolute bottom-[28.5vw] text-[2vw] text-white">
+            <div v-if="jogadaVencedora">
               {{ jogadorVencedorComputed }} ganhou com a carta: {{ cartaVencedoraComputed }}
-            </span>
-            <span v-if="passandoPonto">
+            </div>
+            <div v-if="passandoPonto">
               1 ponto para o time {{ timeVencedorRodada }}
-            </span>
+            </div>
           </div>
           <div class="flex flex-row">
             <div v-for="carta in cartasPlayer1" @click="jogaCarta(carta, 1)">
@@ -364,7 +387,6 @@ onMounted(() => {
       <div class="absolute bottom-[1.5vw] mr-[5vw] text-[1vw] text-white text-right">
         <div class="">1º rodada:<span v-if="timeVencedorTurno1"> Time {{ timeVencedorTurno1 }} </span></div>
         <div class="">2º rodada:<span v-if="timeVencedorTurno2"> Time {{ timeVencedorTurno2 }} </span></div>
-        <div class="">3º rodada:<span v-if="timeVencedorTurno3"> Time {{ timeVencedorTurno3 }} </span></div>
       </div>
     </div>
   </div>
